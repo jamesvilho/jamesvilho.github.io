c18c4abe02bd51b8c35b6d16659f77af8934c721
James Vilho <92689432+jamesvilho@users.noreply.github.com>
2025-11-04 05:05:41 +0800

    Update build.py

--- build.py
+++ build.py
@@ -55,7 +55,7 @@ def get_author_dict():
         'Simon Gerstenecker': 'https://www.linkedin.com/in/simon-gerstenecker/',
         }
 
-def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Bernhard Jaeger', add_links=True):
+def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Vilho James', add_links=True):
     links = get_author_dict() if add_links else {}
     s = ""
     for p in persons:
